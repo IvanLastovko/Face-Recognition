27be733b14ec2ba69fe35015cd7af47223a374dd
lastovko97@gmail.com <lastovko97@gmail.com>
2020-07-28 17:40:30 +0300

    Updates

---
+++ precache-manifest.36b336d74a9e35b18e5060e5fb10d7a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454154fb54688ceae32dc7b9fd2412dd",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "1d1b1ceb248ea1b3eed0",
+    "url": "/Face-Recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9b2e0b9e131278008d9",
+    "url": "/Face-Recognition/static/css/main.22da39eb.chunk.css"
+  },
+  {
+    "revision": "1d1b1ceb248ea1b3eed0",
+    "url": "/Face-Recognition/static/js/2.9ec8e9d0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Face-Recognition/static/js/2.9ec8e9d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b2e0b9e131278008d9",
+    "url": "/Face-Recognition/static/js/main.4777637a.chunk.js"
+  },
+  {
+    "revision": "c6a6524934ca570340e0",
+    "url": "/Face-Recognition/static/js/runtime-main.b8ffc184.js"
+  }
+]);
